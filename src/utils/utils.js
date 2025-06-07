5b512cfc88b566df4e03accd6d20f59c75ed09ed
Harsh-Anand-20-06 <anandbrothers1000@gmail.com>
2025-06-07 17:52:44 +0530

    connected db

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -19,11 +19,11 @@ export const generateToken = (userId, res) => {
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb://127.0.0.1:27017/mydatabase");
-
+        const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URI);
         console.log(`MongoDB connected on host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection failed:", error);
-        process.exit(1); 
+        process.exit(1);
     }
 };
+
